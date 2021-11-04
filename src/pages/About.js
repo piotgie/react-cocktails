33c2246382ff93786345a819e5202089d17ac04f
Piotr Gaik <piotgie@gmail.com>
2021-11-04 18:07:19 +0100

    init

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis aliquam impedit incidunt explicabo adipisci quidem ducimus repellendus, ut, quae beatae alias cumque corporis? Repudiandae error illo obcaecati doloremque officiis ratione aliquam officia asperiores beatae. Maxime odit quam voluptatum cum earum.
+      </p>
+    </section>
+  )
+}
+
+export default About
